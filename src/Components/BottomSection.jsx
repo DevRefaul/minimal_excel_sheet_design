d7ae851083d48fe079b64b0fc8da4afd7efe11bb
DevRefaul <devrefaul@gmail.com>
2023-02-05 12:31:02 +0600

    created bottom section

--- src/Components/BottomSection.jsx
+++ src/Components/BottomSection.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const BottomSection = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Created bottom section</h2>
+    </div>
+  );
 };
 
 export default BottomSection;
