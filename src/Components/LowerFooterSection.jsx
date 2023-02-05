da0ede46c7ef8dc470cb3dcab949a4f0652fc876
DevRefaul <devrefaul@gmail.com>
2023-02-05 13:10:28 +0600

    added some basic buttons

--- src/Components/LowerFooterSection.jsx
+++ src/Components/LowerFooterSection.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const LowerFooterSection = () => {
-  return <div></div>;
+    return (
+      <section>
+        {/* top buttons */}
+        <div className="lg:w-[80%] mx-auto flex justify-between">
+          <div>
+            <button>Save</button>
+            <button>Cancel</button>
+          </div>
+          <div>
+            <button>Delete</button>
+            <button>New</button>
+            <button>Print</button>
+            <button>Prev</button>
+            <button>Nxt</button>
+            <button>Export</button>
+            <button>Import</button>
+          </div>
+        </div>
+
+        {/* bottom buttons */}
+        <div></div>
+      </section>
+    );
 };
 
 export default LowerFooterSection;
